488d06912335c11449f54f65e2de85103c0a9240
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-10-26 15:00:18 -0400

    Updated BWA scripts, restarting it.
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

--- BWA/Run0.sh
+++ BWA/Run0.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-#PBS -N bwa-2012-10-11.9-xaa
-#PBS -o bwa-2012-10-11.9-xaa.stdout
-#PBS -e bwa-2012-10-11.9-xaa.stderr
+#PBS -N bwa+samtools-2012-10-27.99-xaa
+#PBS -o bwa+samtools-2012-10-27.99-xaa.stdout
+#PBS -e bwa+samtools-2012-10-27.99-xaa.stderr
 #PBS -A six-213-ad
 #PBS -q debug1
+#PBS -l hostlist=r105-n57
+##########PBS -l hostlist=r105-n57+r105-n58+r105-n67+r105-n68
+#####r105-n[57,58,67,68]
 #PBS -l qos=DebugQ 
-#PBS -l walltime=48:00:00
+#PBS -l walltime=8:00:00
 #PBS -l nodes=1:ppn=8
-#PBS -l hostlist=r103-n56
 
 cd $PBS_O_WORKDIR
 
-source Load.sh
+. Load.sh
 
 for group in $(cat Groups.xaa)
 do
-	./RunGroup.sh Reference.fasta $group Storage/bwa-2012-10-11.9-xaa &
+	./RunGroup.sh Reference.fasta $group Storage/bwa+samtools-2012-10-27.99-xaa &
 done
 
 wait
--- BWA/Start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in $(seq 0 3)
-do
-	msub Run$i.sh
-done
