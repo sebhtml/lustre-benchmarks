3fd1bf42ce869ae27c4a4900ff9257ea705afc99
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-10-26 15:00:40 -0400

    Improved the test with GNU sort.
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

--- GNU-coreutils-sort/Program.sh
+++ GNU-coreutils-sort/Program.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-storage=../selected-lustre/sort-2012-10-25.1
+#PBS -N sort-2012-10-27.1
+#PBS -o sort-2012-10-27.1.stdout
+#PBS -e sort-2012-10-27.1.stderr
+#PBS -A six-213-ad
+#PBS -q debug1
+#PBS -l hostlist=r105-n57+r105-n58+r105-n67+r105-n68
+#####r105-n[57,58,67,68]
+#PBS -l qos=DebugQ 
+#PBS -l walltime=8:00:00
+#PBS -l nodes=4:ppn=8
+
+
+storage=../selected-lustre/sort-2012-10-27.1
 
 mkdir $storage
 cd $storage
