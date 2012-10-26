059d1d811027e8024b763b9e98ee654b76c0b435
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-10-26 11:59:38 -0400

    Splitted test files.
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

--- IOR/job.sh
+++ IOR/job.sh
@@ -1,27 +1,55 @@
 #!/bin/bash
 
-#PBS -N IOR-2012-10-01
-#PBS -o IOR-2012-10-01.stdout
-#PBS -e IOR-2012-10-01.stderr
+#PBS -N IOR-2012-10-27.1
+#PBS -o IOR-2012-10-27.1.stdout
+#PBS -e IOR-2012-10-27.1.stderr
 #PBS -A nne-790-aa
 #PBS -q debug1
-#PBS -l hostlist=r109-n[3-6]
+#PBS -l hostlist=r105-n{57,58,67,68}
 #PBS -l qos=DebugQ 
-#PBS -l walltime=1:00:00
+#PBS -l walltime=8:00:00
 #PBS -l nodes=4:ppn=8
 
 cd $PBS_O_WORKDIR
 
-NSLOTS=32
+coresPerNode=8
+nodes=4
+
+
+NSLOTS=$(($nodes * $coresPerNode))
+maximumCores=NSLOTS
 
 . /rap/clumeq/Seb-Boisvert/software/IOR-2.10.3/Load.sh
 
-rm StorageAppliance
+jobDirectory=../selected-lustre/IOR-2012-10-27.1
 
-for storage in xyratex-lustre # default-lustre
+mkdir $jobDirectory
+
+# -1 means all OSTs
+for stripeCount in 1 2 4 8 -1
 do
-	ln -s $storage StorageAppliance
-	mpiexec -n $NSLOTS IOR -f tests.ior &> $storage.txt
-	rm StorageAppliance
+	# Jean-François only wants 1M for stripeSize
+	for stripeSize in 1M 
+	do
+		directory=$jobDirectory/stripeSize=$stripeSize,stripeCount=$stripeCount
+		mkdir $directory
+		lfs setstripe -s $stripeSize -i 0 -c $stripeCount $directory
+
+		for NSLOTS in $(seq $coresPerNode $coresPerNode $maximumCores)
+		do
+			jobWithNodes=$directory/$NSLOTS
+			mkdir $jobWithNodes
+
+			ln -s $jobWithNodes storage
+
+			for testCase in $(ls ior-tests/*.ior)
+			do
+				mpiexec -n $NSLOTS IOR -f $testCase &> storage/000-$(basename $testCase).txt
+			done
+
+			rm storage
+		done
+	done
 done
+
 
