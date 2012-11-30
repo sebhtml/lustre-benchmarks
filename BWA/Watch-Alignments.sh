b2980c2cc2a123415b06be62cffa07abe392a64f
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-11-30 15:54:47 -0500

    Added progress for indexing
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

--- BWA/Watch-Alignments.sh
+++ BWA/Watch-Alignments.sh
@@ -1,17 +1,72 @@
 #!/bin/bash
 
+function printProgressBar(){
+	progress=$1
+	for j in $(seq 1 $progress)
+	do
+		echo -n "*"
+	done
+}
+
+function printDate(){
+
+	theFile=$1
+
+	theTime=$(stat $theFile|grep Modify|awk '{print $3}'|sed 's/\./ /g'|awk '{print $1}')
+	echo -n "$theTime  "
+
+}
 
 while true
 do
 	clear
 
-	echo "Process                                     Modification                           Completed"
+	echo "Indexing"
+	echo ""
+
+	for i in $(ls|grep bwa+samtools|grep index)
+	do
+		progress=$(cat $i|grep iterations|wc -l)
+		total="?"
+		echo -n "$i "
+
+		printDate $i
+
+		printf "[%2s/%2s] " $progress $total
+		printProgressBar $progress
+
+		if test $(grep Finished $i|wc -l) -eq 1
+		then
+			echo -n " [X]" &> /dev/null
+		else
+			echo -n " [ ]" &> /dev/null
+		fi
+		
+		echo ""
+	done
+
+	echo ""
+
+	echo "Alignment"
+	echo ""
+
 	for i in $(ls|grep bwa+samtools|grep File)
 	do
-		echo -n "$i    "
-		theTime=$(stat $i|grep Modify|awk '{print $2" "$3" "$4}')
-		echo -n "$theTime    "
-		grep "bwa aln" $i |wc -l
+		echo -n "$i  "
+
+		printDate $i
+
+		progress=$(grep "bwa aln" $i |wc -l)
+
+		content=File$(echo $i|sed 's/File/ /g'|awk '{print $2}')
+
+		total=$(cat $content|wc -l)
+
+		printf "[%2s/%2s] " $progress $total
+
+		printProgressBar $progress
+
+		echo ""
 	done
 
 	sleep 10
