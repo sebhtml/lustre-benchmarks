7593add7a78788c214a36738617890f1a6178862
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-11-30 15:14:31 -0500

    Enhanced watcher
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

--- BWA/Watch-Alignments.sh
+++ BWA/Watch-Alignments.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-while true; do clear; for i in $(ls|grep bwa+samtools|grep File); do echo -n "$i "; grep "bwa aln" $i |wc -l; done; sleep 10; done
+
+while true
+do
+	clear
+
+	echo "Process                                     Modification                           Completed"
+	for i in $(ls|grep bwa+samtools|grep File)
+	do
+		echo -n "$i    "
+		theTime=$(stat $i|grep Modify|awk '{print $2" "$3" "$4}')
+		echo -n "$theTime    "
+		grep "bwa aln" $i |wc -l
+	done
+
+	sleep 10
+
+done
