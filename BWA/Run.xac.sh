9919feed3995ac6422b46994a03df13b4b76402c
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-12-01 12:13:46 -0500

    Started Xyratex BWA jobs
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

--- BWA/Run.xac.sh
+++ BWA/Run.xac.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#PBS -N bwa+samtools-Oracle-2012-12-01-1-xac
-#PBS -o bwa+samtools-Oracle-2012-12-01-1-xac.stdout
-#PBS -e bwa+samtools-Oracle-2012-12-01-1-xac.stderr
+#PBS -N bwa+samtools-Xyratex-2012-12-01-1-xac
+#PBS -o bwa+samtools-Xyratex-2012-12-01-1-xac.stdout
+#PBS -e bwa+samtools-Xyratex-2012-12-01-1-xac.stderr
 #PBS -A six-213-ad
 #PBS -q debug1
-#PBS -l hostlist=r110-n5
+#PBS -l hostlist=r105-n67
 #PBS -l qos=DebugQ 
 #PBS -l walltime=48:00:00
 #PBS -l nodes=1:ppn=8
 
 cd $PBS_O_WORKDIR
 
-./Run-job.sh ../selected-lustre bwa+samtools-Oracle-2012-12-01-1 xac
+./Run-job.sh ../selected-lustre bwa+samtools-Xyratex-2012-12-01-1 xac
