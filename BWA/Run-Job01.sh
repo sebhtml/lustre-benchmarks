328940b0c2aa12ae020f92e4e4e34eafd742a650
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-12-06 14:51:52 -0500

    New launch scripts
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

---
+++ BWA/Run-Job01.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#PBS -N bwa+samtools-Oracle-2012-12-06-2-Job01
+#PBS -o bwa+samtools-Oracle-2012-12-06-2-Job01.stdout
+#PBS -e bwa+samtools-Oracle-2012-12-06-2-Job01.stderr
+#PBS -A six-213-ad
+#PBS -q debug1
+##PBS -l hostlist=r105-n57
+#PBS -l qos=DebugQ 
+#PBS -l walltime=48:00:00
+#PBS -l nodes=1:ppn=8
+
+cd $PBS_O_WORKDIR
+
+./Run-job.sh ../selected-lustre bwa+samtools-Oracle-2012-12-06-2 Job01
